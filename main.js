499e61a0f0b02bae6e7de5355b36c035910969b9
BuildTools <zsoltika192@gmail.com>
2024-08-01 21:30:04 +0200

    1.5 Add feedback panel, make clean deasign,add songs

--- main.js
+++ main.js
@@ -3,22 +3,25 @@
 
 // Songs database
 var songs = [
-    { title: "Blinding Lights", year: 2019,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Save Your Tears", year: 2020,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Alone Again", year: 2019,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Too Late", year: 2019,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Hardest to Love", year: 2020,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Scared to Live", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Snowchild", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Escape from LA", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Heartless", year: 2020,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Faith", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "In Your Eyes", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "Repeat After Me (Interlude)", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
-    { title: "After Hours", year: 2019,cover:"img/afterhours_theweeknd.jpg" },
-    { title: "Until I Bleed Out", year: 2020,cover:"img/afterhours_theweeknd.jpg" },
-    {title: "Starboy", year:2016, cover: "img/The_Weeknd_-_Starboy.png"},
-    {title: "Dawn FM",year: 2022,cover:"img/weeknd.jpeg.jpeg"}
+    { album: "After Hours",title: "Blinding Lights", year: 2019,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours",title: "Save Your Tears", year: 2020,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours",title: "Alone Again", year: 2019,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Too Late", year: 2019,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Hardest to Love", year: 2020,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Scared to Live", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Snowchild", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Escape from LA", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Heartless", year: 2020,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Faith", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "In Your Eyes", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "Repeat After Me (Interlude)", year: 2019 ,cover:"img/afterhours_theweeknd.jpg"},
+    { album: "After Hours", title: "After Hours", year: 2019,cover:"img/afterhours_theweeknd.jpg" },
+    { album: "After Hours", title: "Until I Bleed Out", year: 2020,cover:"img/afterhours_theweeknd.jpg" },
+    { album: "Starboy",title: "Starboy", year:2016, cover: "img/The_Weeknd_-_Starboy.png"},
+    { album: "Starboy",title: "Party Monster",year: 2016,cover:"img/The_Weeknd_-_Starboy.png"},
+    { album: "Starboy",title:"False Alarm",year: 2016,cover:"img/The_Weeknd_-_Starboy.png"},
+    { album: "Starboy",title:"Reminder",year:2016,cover:"img/The_Weeknd_-_Starboy.png"},
+     {album:"Dawn FM", title: "Dawn FM",year: 2022,cover:"img/weeknd.jpeg.jpeg"}
     // Add more songs from the "After Hours" album
 ];
 
@@ -41,6 +44,8 @@ function handleSongSelection() {
     var selectedSong = songs.find(function(song) {
         return song.title === selectedSongTitle;
     });
+
+
     if (selectedSong) {
         var songDetail = document.getElementById("songDetail");
         songDetail.innerHTML = "Selected song: " + selectedSong.title + " (" + selectedSong.year + ")";
@@ -49,14 +54,13 @@ function handleSongSelection() {
         coverImage.src = selectedSong.cover;
 
         var realFan = document.getElementById("realFan")
-         if(selectedSong.year > 2019 && selectedSong.year < 2018){
+         if(selectedSong.year > 2020 && selectedSong.year > 2020){
         realFan.innerHTML = "Your not a real fan :("
     }else{
         realFan.innerHTML ="You are a real fan"
     };
 }
 }
-
 // Populate the select dropdown when the page loads
 document.addEventListener("DOMContentLoaded", function() {
     populateSongSelector();
